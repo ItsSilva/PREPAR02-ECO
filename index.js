e1557414d115ba5c3b1c1b0761e2c064b783d464
Juan Silva <juanse909@hotmail.com>
2025-03-27 00:59:59 -0500

    feat: login and post methods created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const { Server } = require("socket.io");
 const { createServer } = require("http");
+const { log } = require("console");
 
 const app = express();
 const httpServer = createServer(app);
@@ -17,11 +18,14 @@ app.use("/app1", express.static(path.join(__dirname, "app1")));
 app.use("/app2", express.static(path.join(__dirname, "app2")));
 
 let users = [];
+let posts = [];
 
+//get route for users
 app.get("/users", (req, res) => {
   res.send(users);
 });
 
+//post route for register
 app.post("/users", (req, res) => {
   const dataUser = req.body;
 
@@ -46,6 +50,54 @@ app.post("/users", (req, res) => {
   res.status(201).json(newUser);
 
   io.emit("new-user", newUser);
+});
+
+//post route for login
+app.post("/login", (req, res) => {
+  const dataUser = req.body;
+
+  if (!dataUser.email || !dataUser.password) {
+    return res.status(400).send({ message: "All fields are required" });
+  }
+
+  const user = users.find(
+    (user) =>
+      user.email === dataUser.email && user.password === dataUser.password
+  );
+
+  if (!user) {
+    return res.status(400).send({ message: "Invalid credentials" });
+  }
+
+  res.status(200).json(user);
+});
+
+//get route for posts
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+//post route for posts
+app.post("/posts", (req, res) => {
+  const dataPost = req.body;
+
+  if (!dataPost.title || !dataPost.description || !dataPost.image) {
+    return res.status(400).send({ message: "All fields are required" });
+  }
+
+  const postId = Math.floor(Math.random() * 1000000);
+
+  const newPost = {
+    id: postId,
+    image: dataPost.image,
+    title: dataPost.title,
+    description: dataPost.description,
+  };
+
+  posts.push(newPost);
+  res.status(201).json(newPost);
+
+  io.emit("new-post", newPost);
 });
 
 httpServer.listen(5050);
