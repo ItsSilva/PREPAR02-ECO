f30c277b184f3de3a47fd8701df0c8ab9bb4e261
Juan Silva <juanse909@hotmail.com>
2025-03-27 01:00:29 -0500

    feat: realtime update socket added for post

--- app2/index.js
+++ app2/index.js
@@ -1,5 +1,6 @@
 const socket = io("http://localhost:5050", { path: "/rea-time" });
 
+//Create Users Cards
 userCard = async () => {
   const usersContainer = document.getElementById("users-container");
   usersContainer.innerHTML = "";
@@ -18,9 +19,35 @@ userCard = async () => {
   });
 };
 
+//create Posts Cards
+const postCard = async () => {
+  const postsContainer = document.getElementById("posts-container");
+  postsContainer.innerHTML = "";
+
+  const response = await fetch("http://localhost:5050/posts");
+  const data = await response.json();
+
+  data.forEach((post) => {
+    const postCard = document.createElement("div");
+    postCard.className = "post-card";
+    postCard.innerHTML = `
+      <img src="${post.image}" alt="${post.title}" />
+      <h3>${post.title}</h3>
+      <p>${post.description}</p>
+    `;
+    postsContainer.appendChild(postCard);
+  });
+};
+
 //Socket.io
+//New User
 socket.on("new-user", (data) => {
   console.log("New user:", data);
   userCard();
 });
-//
+
+//New Post
+socket.on("new-post", (data) => {
+  console.log("New post:", data);
+  postCard();
+});
